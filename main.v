9c20a53e239b71dce05b75861bcc83054c32a8cb
Pranav Sapkale <pransomit@gmail.com>
2026-03-22 14:13:56 +0530

    Final Update

--- main.v
+++ main.v
@@ -4,12 +4,13 @@
 module main(
 input [7:0] in1,in2,
 input [3:0] sel,
+input dir,cin,
 output reg [7:0] out,
 output reg [4:0] flags
     );
     
-    wire [7:0] caseresult[13:0];
-    wire [4:0] caseflags[13:0];
+    wire [7:0] caseresult[12:0];
+    wire [4:0] caseflags[12:0];
             cla8b case1 (.in1(in1),.in2(in2),.cin(1'b0),.out(caseresult[0]),.flags(caseflags[0]));
             cla8b case2 (.in1(in1),.in2(in2),.cin(1'b1),.out(caseresult[1]),.flags(caseflags[1]));
               sub case3 (.in1(in1),.in2(in2),.out(caseresult[2]),.flags(caseflags[2])); 
@@ -17,6 +18,12 @@ output reg [4:0] flags
          twoscomp case5 (.in1(in1),.out(caseresult[4]),.flags(caseflags[4]));
         increment case6 (.in1(in1),.out(caseresult[5]),.flags(caseflags[5]));
         decrement case7 (.in1(in1),.out(caseresult[6]),.flags(caseflags[6]));
+         and_gate case8 (.in1(in1),.in2(in2),.out(caseresult[7]),.flags(caseflags[7]));
+          or_gate case9 (.in1(in1),.in2(in2),.out(caseresult[8]),.flags(caseflags[8]));
+         xor_gate case10 (.in1(in1),.in2(in2),.out(caseresult[9]),.flags(caseflags[9]));
+    logical_shift case11 (.in1(in1),.dir(dir),.out(caseresult[10]),.flags(caseflags[10]));
+          rotator case12 (.in1(in1),.dir(dir),.out(caseresult[11]),.flags(caseflags[11]));
+    rotator_carry case13 (.in1(in1),.cin(cin),.dir(dir),.out(caseresult[12]),.flags(caseflags[12]));
     always @ (*)
     begin
     case (sel) 
@@ -27,13 +34,12 @@ output reg [4:0] flags
     4'b0100 : begin  out = caseresult[4]; flags = caseflags[4]; end
     4'b0101:  begin  out = caseresult[5]; flags = caseflags[5]; end
     4'b0110:  begin  out = caseresult[6]; flags = caseflags[6]; end
-    4'b0111: ;
-    4'b1000: ;
-    4'b1001: ;
-    4'b1010: ;
-    4'b1011: ;
-    4'b1100: ;
-    4'b1101: ;
+    4'b0111:  begin  out = caseresult[7]; flags = caseflags[7]; end
+    4'b1000:  begin  out = caseresult[8]; flags = caseflags[8]; end
+    4'b1001:  begin  out = caseresult[9]; flags = caseflags[9]; end
+    4'b1010:  begin  out = caseresult[10]; flags = caseflags[10]; end
+    4'b1011:  begin  out = caseresult[11]; flags = caseflags[11]; end
+    4'b1100:  begin  out = caseresult[12]; flags = caseflags[12]; end
     default: ;
     endcase
     end
